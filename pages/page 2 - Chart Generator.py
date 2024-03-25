c4939b5eeb92aa53b9c91de01ad16d681921d712
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 22:22:15 -0700

    Update page 2 - Chart Generator.py

--- pages/page 2 - Chart Generator.py
+++ pages/page 2 - Chart Generator.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.header("Welcome to the Chart Generator")
-st.subheader(f"Here is a chart displaying information about {st.session_state["name_of_company"]}")
 try:
+    st.write(f"Here is a chart displaying information about {st.session_state["name_of_company"]}")
     df = pd.DataFrame(st.session_state["user_input"])
 except KeyError:
     st.error('The requested key does not exist in the session state.\n' 
