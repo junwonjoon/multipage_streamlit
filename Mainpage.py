53136c66ee12e2c817e77d5bf89ade8d711aedc0
Wonjoon <junwonjoon41@gmail.com>
2024-03-25 01:32:12 -0700

    Update Mainpage.py

--- Mainpage.py
+++ Mainpage.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 
 st.set_page_config(
-    page_title="Multipage App",
-    page_icon = ":)"
+    page_title="Wonjoon's Stock Graph",
+    page_icon = "📊"
 )
 def generate_stock_pandas(dict_stocksTicker:dict, stocksTicker_select:str, timespan_multiplier_select:int, timespan_select:str,
                    start_date_select:datetime, end_date_select:datetime, key:str = st.secrets["API_KEY"]) -> dict:
@@ -35,7 +35,7 @@ def generate_stock_pandas(dict_stocksTicker:dict, stocksTicker_select:str, times
         return pd.DataFrame(data)
 
 st.title("Main Page")
-
+st.captions("Please keep that in mind that this uses a free API, therefore if you make too many request under one minute, the program might not function. Also, please keep that in mind that using multichart is one API call per selection!")
 
 dict_stocksTicker ={"Apple Inc.": "AAPL",
                     "Microsoft Corporation": "MSFT",
