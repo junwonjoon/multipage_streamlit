a4651da7e014cd47c861de33f4187395c7469eb2
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:42:07 -0700

    Update Page 1 - Table Generator.py

--- pages/Page 1 - Table Generator.py
+++ pages/Page 1 - Table Generator.py
@@ -1,12 +1,10 @@
 import streamlit as st
 from requests import get
-import datetime
 import pandas as pd
 
 
-if st.button("Generate Table", type="primary"):
-    df = pd.DataFrame(st.session_state["user_input"])
-    st.table(df)
+df = pd.DataFrame(st.session_state["user_input"])
+st.table(df)
 
    
 
