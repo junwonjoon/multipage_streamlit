e63d7e8c9664e0f1d4f6fa87b3feeb76eae562d2
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 22:30:38 -0700

    Added page links

--- pages/page_2_-_Chart_Generator.py
+++ pages/page_2_-_Chart_Generator.py
@@ -1,18 +1,21 @@
 import streamlit as st
-from requests import get
 import pandas as pd
 
-st.header("Welcome to The Table Generator")
+st.header("Welcome to the Chart Generator")
 try:
-    st.write(f'Here is a table displaying information about {st.session_state["name_of_company"]}')
+    st.write(f'Here is a chart displaying information about {st.session_state["name_of_company"]}')
     df = pd.DataFrame(st.session_state["user_input"])
 except KeyError:
     st.error('The requested key does not exist in the session state.\n' 
              'Please go back to the main page to save your preference or try sometime later')
-else:
-    st.table(df)
+    st.page_link("Mainpage.py", label="Home", icon="🏠")
 
-   
+else:
+    df.set_index('Time', inplace=True)
+    st.line_chart(df)
+
+
+
 
 
 
