16c1355269cd7daac97a7a288925447ba55f57ef
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:20:33 -0700

    Update Mainpage.py

--- Mainpage.py
+++ Mainpage.py
@@ -7,7 +7,7 @@ def displayDelta(stocksTicker:str, startday:str, endday:str) ->None:
     json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/1/day/{startday}/{endday}?apiKey={key}").json()
     if json_data["status"] == "ERROR":
         st.header("Hi welcome to the stocks page")
-        pass
+        return 0
     else:
         average_stock_price = [element["vw"] for element in json_data["results"]]
         st.write(average_stock_price)
@@ -18,8 +18,8 @@ def displayDelta(stocksTicker:str, startday:str, endday:str) ->None:
 today = datetime.datetime.now() - datetime.timedelta(days=1)
 two_days_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 
-displayDelta("AAPL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d')))
-displayDelta("GOOGL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d')))
+if displayDelta("AAPL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d'))) != 0:
+    displayDelta("GOOGL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d')))
 
 st.set_page_config(
     page_title="Multipage App",
