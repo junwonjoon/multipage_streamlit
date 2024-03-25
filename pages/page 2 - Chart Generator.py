7b16899db7fef49897bfa21814e9e70cda43ba59
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:57:13 -0700

    Update page 2 - Chart Generator.py

--- pages/page 2 - Chart Generator.py
+++ pages/page 2 - Chart Generator.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.header("Welcome to the chart generator")
 df2 = pd.DataFrame(st.session_state["user_input"])
 df2.set_index('Time', inplace=True)
-st.line_chart(df, color = '#ef4423')
+st.line_chart(df2, color = '#ef4423')
 
 
 
