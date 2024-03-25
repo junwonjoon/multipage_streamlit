bc2973413e0eeea66526c681622aeb95def8e7be
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:47:44 -0700

    Update Page 1 - Table Generator.py

--- pages/Page 1 - Table Generator.py
+++ pages/Page 1 - Table Generator.py
@@ -2,7 +2,6 @@ import streamlit as st
 from requests import get
 import pandas as pd
 
-st.header(f"displaying information about {st.session_state["user_input"][1]}")
 df = pd.DataFrame(st.session_state["user_input"][0])
 st.table(df)
 
