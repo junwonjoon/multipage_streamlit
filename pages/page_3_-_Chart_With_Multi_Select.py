5948c99b084aa340c269d51c537804df74528f5b
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 23:39:31 -0700

    Create page_3_-_Chart_With_Multi_Select.py

---
+++ pages/page_3_-_Chart_With_Multi_Select.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from Mainpage import generate_stock_dictionary, dict_stocksTicker
+
+
+st.header("Welcome to the Multi Select Chart Generator")
+options = st.multiselect(
+    'Select the prices that you want to compare',
+    [keys for keys in dict_stocksTicker.keys()])
+
+st.write('You selected:', options)
+
+
+
+
+
+
+
+
