6fcfa93d5e243ece6a68ac41a166b328485af194
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 23:42:32 -0700

    Update page_3_-_Chart_With_Multi_Select.py

--- pages/page_3_-_Chart_With_Multi_Select.py
+++ pages/page_3_-_Chart_With_Multi_Select.py
@@ -6,7 +6,9 @@ from Mainpage import generate_stock_dictionary, dict_stocksTicker
 st.header("Welcome to the Multi Select Chart Generator")
 options = st.multiselect(
     'Select the prices that you want to compare',
-    [keys for keys in dict_stocksTicker.keys()])
+    [keys for keys in dict_stocksTicker.keys()],
+    default = None, max_selections = 3,
+    placeholder = st.session_state["name_of_company"])
 
 st.write('You selected:', options)
 
