82263c170ce792abd9c42cff44eaf86b6d86d85d
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:27:43 -0700

    Update Mainpage.py

--- Mainpage.py
+++ Mainpage.py
@@ -15,7 +15,6 @@ def displayDelta(stocksTicker:str, startday:str, endday:str) ->None:
         return 0
     else:
         average_stock_price = [element["vw"] for element in json_data["results"]]
-        st.write(average_stock_price)
         if len(average_stock_price) > 2:
             delta_to_print = float(average_stock_price[-1]) - float(average_stock_price[-2])
             st.metric(label=f"{stocksTicker} compared to yesterday", value=average_stock_price[-1], delta=delta_to_print)
@@ -23,7 +22,7 @@ st.title("Main Page")
 
 st.sidebar.success("Select a page above.")
 
-today = datetime.datetime.now() - datetime.timedelta(days=1)
+today = datetime.datetime.now()
 two_days_ago = datetime.datetime.now() - datetime.timedelta(days=7)
 
 if displayDelta("AAPL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d'))) != 0:
