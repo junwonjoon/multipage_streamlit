6754f70fb4ea42389ae213fbb0679730eb01c042
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 22:04:18 -0700

    added error message and implemented try in page 2

--- pages/Page 1 - Table Generator.py
+++ pages/Page 1 - Table Generator.py
@@ -6,9 +6,8 @@ st.header("Welcome to the table generator")
 try:
     df = pd.DataFrame(st.session_state["user_input"])
 except KeyError:
-    # Handle the case where the key does not exist
-    st.error('The requested key does not exist in the session state.')
-    # Optionally, set a default value or perform other fallback operations
+    st.error('The requested key does not exist in the session state.\n' 
+             'Please go back to the main page to save your preference or try sometime later')
 else:
     st.table(df)
 
--- pages/page 2 - Chart Generator.py
+++ pages/page 2 - Chart Generator.py
@@ -3,9 +3,15 @@ from requests import get
 import pandas as pd
 
 st.header("Welcome to the chart generator")
-df2 = pd.DataFrame(st.session_state["user_input"])
-df2.set_index('Time', inplace=True)
-st.line_chart(df2, color = '#ef4423')
+
+try:
+    df2 = pd.DataFrame(st.session_state["user_input"])
+except KeyError:
+    st.error('The requested key does not exist in the session state.\n' 
+             'Please go back to the main page to save your preference or try sometime later')
+else:
+    df2.set_index('Time', inplace=True)
+    st.line_chart(df2, color = '#ef4423')
 
 
 
