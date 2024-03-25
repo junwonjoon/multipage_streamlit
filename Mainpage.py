58700b4baeec4c3933e3695046fae4e575894a75
Wonjoon <junwonjoon41@gmail.com>
2024-03-25 01:29:13 -0700

    Update Mainpage.py

--- Mainpage.py
+++ Mainpage.py
@@ -108,7 +108,7 @@ if st.button("Save", type="primary"):
         st.write("Your choice have been saved, now navigate to different page to view your results")
         st.page_link("pages/Page_1_-_Table_Generator.py", label="Table", icon="📊")
         st.page_link("pages/page_2_-_Chart_Generator.py", label="Chart", icon="📈")
-        st.page_link("pages/page_3_-_Chart_With_Multi_Select.py", label="Multi Chart", icon="📈📈📈")
+        st.page_link("pages/page_3_-_Chart_With_Multi_Select.py", label="Multi Chart", icon="📈")
 
 
 
