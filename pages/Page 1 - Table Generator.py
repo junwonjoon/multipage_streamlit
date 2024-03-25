60f8b4a6506e51b6518311eec2716a75f3ff70f3
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:44:47 -0700

    Update Page 1 - Table Generator.py

--- pages/Page 1 - Table Generator.py
+++ pages/Page 1 - Table Generator.py
@@ -2,7 +2,7 @@ import streamlit as st
 from requests import get
 import pandas as pd
 
-
+st.header(f"displaying information about {st.session_state["company_name"]}")
 df = pd.DataFrame(st.session_state["user_input"])
 st.table(df)
 
