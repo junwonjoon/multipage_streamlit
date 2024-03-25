72396076a724acbda29d669d4157fafb0a107299
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:54:56 -0700

    Update page 2 - Chart Generator.py

--- pages/page 2 - Chart Generator.py
+++ pages/page 2 - Chart Generator.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from requests import get
-import datetime
 import pandas as pd
 
-
+st.header("Welcome to the table generator")
 df = pd.DataFrame(st.session_state["user_input"])
 df.set_index('Time', inplace=True)
 st.line_chart(df, color = '#ef4423')
