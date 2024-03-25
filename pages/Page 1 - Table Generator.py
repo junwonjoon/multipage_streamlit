cbd077f03b472ed7029bc2e3c645cbd088452013
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:51:46 -0700

    Update Page 1 - Table Generator.py

--- pages/Page 1 - Table Generator.py
+++ pages/Page 1 - Table Generator.py
@@ -2,7 +2,7 @@ import streamlit as st
 from requests import get
 import pandas as pd
 
-df = pd.DataFrame(st.session_state["user_input"][0])
+df = pd.DataFrame(st.session_state["user_input"])
 st.table(df)
 
    
