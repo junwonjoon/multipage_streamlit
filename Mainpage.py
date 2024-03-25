7ddeafb218b07ab143ff6a75239cf3cae7a40917
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 21:10:43 -0700

    Update Mainpage.py

--- Mainpage.py
+++ Mainpage.py
@@ -2,7 +2,8 @@ import streamlit as st
 from requests import get
 import datetime
 
-def displayDelta(stocksTicker:str, startday:str, endday:str, key:str) ->None:
+def displayDelta(stocksTicker:str, startday:str, endday:str) ->None:
+    key = st.secrets["API_KEY"]
     json_data = get(f"https://api.polygon.io/v2/aggs/ticker/{stocksTicker}/range/1/day/{startday}/{endday}?apiKey={key}").json()
     if json_data["status"] == "ERROR":
         st.write("Too many request were created, maximum request is 5 per minute, try again a minute later")
@@ -10,15 +11,14 @@ def displayDelta(stocksTicker:str, startday:str, endday:str, key:str) ->None:
         st.write("Sorry, the range you have assigned contain too many steps! Please reduce the range of steps by increasing the multiplier or decrease the date difference")
     else:
         average_stock_price = [element["vw"] for element in json_data["results"]]
-        delta_to_print = average_stock_price[-1] - average_stock_price[-2]
+        delta_to_print = float(average_stock_price[-1]) - float(average_stock_price[-2])
         st.metric(label=f"{stocksTicker} compared to yesterday", value=average_stock_price[-1], delta=delta_to_print)
     
 today = datetime.datetime.now() - datetime.timedelta(days=1)
 two_days_ago = datetime.datetime.now() - datetime.timedelta(days=3)
-key = st.secrets["API_KEY"]
 
-displayDelta("AAPL", two_days_ago.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d'), key)
-displayDelta("GOOGL", two_days_ago.strftime('%Y-%m-%d'), today.strftime('%Y-%m-%d'), key)
+displayDelta("AAPL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d')))
+displayDelta("GOOGL", str(two_days_ago.strftime('%Y-%m-%d')), str(today.strftime('%Y-%m-%d')))
 
 st.set_page_config(
     page_title="Multipage App",
