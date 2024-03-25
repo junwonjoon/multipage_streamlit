fa46b16b4dfea8d4dace084a5b94a700b4361c4e
Wonjoon <junwonjoon41@gmail.com>
2024-03-25 01:10:37 -0700

    Update page_3_-_Chart_With_Multi_Select.py

--- pages/page_3_-_Chart_With_Multi_Select.py
+++ pages/page_3_-_Chart_With_Multi_Select.py
@@ -47,11 +47,10 @@ else:
             dfs.append(df)
         st.write(dfs)
         keys = list(dfs[0].keys())
-        # Preparing data for DataFrame creation
+        #This line and below is from chatGPT
         data_for_df = {f'Series{i}': [d[key] for key in keys] for i, d in enumerate(dfs)}
-        # Create a DataFrame
         df = pd.DataFrame(data_for_df, index=pd.to_datetime(keys))
-        st.line_chart(df, y=["red","blue"])
+        st.line_chart(df, color="hello")
 
 
         
