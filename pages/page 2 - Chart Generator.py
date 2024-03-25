5b84b73c588b16fe863c5bca13a4e78b6af00c3d
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 22:13:04 -0700

    Update page 2 - Chart Generator.py

--- pages/page 2 - Chart Generator.py
+++ pages/page 2 - Chart Generator.py
@@ -8,9 +8,8 @@ try:
 except KeyError:
     st.error('The requested key does not exist in the session state.\n' 
              'Please go back to the main page to save your preference or try sometime later')
-else:
-    df2.set_index('Time', inplace=True)
-    st.line_chart(df2)
+df2.set_index('Time', inplace=True)
+st.line_chart(df2)
 
 
 
