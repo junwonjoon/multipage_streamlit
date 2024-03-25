c451f8ae23c1a8d2aae7cd81a32925ac1dcc3ab1
Wonjoon <junwonjoon41@gmail.com>
2024-03-24 22:15:22 -0700

    Update page 2 - Chart Generator.py

--- pages/page 2 - Chart Generator.py
+++ pages/page 2 - Chart Generator.py
@@ -2,14 +2,14 @@ import streamlit as st
 import pandas as pd
 
 st.header("Welcome to the chart generator")
-
 try:
-    df2 = pd.DataFrame(st.session_state["user_input"])
+    df = pd.DataFrame(st.session_state["user_input"])
 except KeyError:
     st.error('The requested key does not exist in the session state.\n' 
              'Please go back to the main page to save your preference or try sometime later')
-df2.set_index('Time', inplace=True)
-st.line_chart(df2)
+else:
+    df.set_index('Time', inplace=True)
+    st.line_chart(df)
 
 
 
