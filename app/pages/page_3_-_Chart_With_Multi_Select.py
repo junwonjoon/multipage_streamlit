e40920b25098b80cf0129d70cdcd2005b6562e60
Wonjoon <junwonjoon41@gmail.com>
2024-03-25 19:07:36 -0700

    Added more error handling

--- app/pages/page_3_-_Chart_With_Multi_Select.py
+++ app/pages/page_3_-_Chart_With_Multi_Select.py
@@ -14,30 +14,38 @@ from requests import get
 def generate_stock_dictionary(dict_stocks_ticker_: dict, stocks_ticker_select: str, timespan_multiplier_select: int,
                               timespan_select: str,
                               start_date_select: datetime, end_date_select: datetime,
-                              key: str = st.secrets["API_KEY"]) -> dict:
+                              key: str) -> dict:
+    if key == "0":
+        key = st.secrets["API_KEY_3"]
     stocks_ticker = dict_stocks_ticker_[stocks_ticker_select]
     multiplier = timespan_multiplier_select
     timespan = timespan_select
     from_date = start_date_select
     to_date = end_date_select
-    json_data = get(
-        f"https://api.polygon.io/v2/aggs/ticker/{stocks_ticker}/range/{multiplier}/"
-        f"{timespan}/{from_date}/{to_date}?apiKey={key}").json()
-    if json_data["status"] == "ERROR":
-        st.write("Too many request were created, maximum request is 5 per minute, try again a minute later")
-        exit()
-    elif json_data["status"] == "NOT_AUTHORIZED":
-        st.write(
-            "Sorry, the range you have assigned contain too many steps! Please reduce the range of steps"
-            " by increasing the multiplier or decrease the date difference")
-        exit()
+    try:
+        json_data = get(
+            f"https://api.polygon.io/v2/aggs/ticker/{stocks_ticker}/range/{multiplier}/"
+            f"{timespan}/{from_date}/{to_date}?apiKey={key}").json()
+    except Exception:
+        st.error("Failed to call API")
+        raise RuntimeError
     else:
-        average_stock_price = [element["vw"] for element in json_data["results"]]
-        the_date_milliseconds = [element["t"] for element in json_data["results"]]
-        human_readable_date = [datetime.datetime.fromtimestamp(element / 1000).strftime('%Y-%m-%d %H:%M:%S') for element
-                               in the_date_milliseconds]
-        data = dict(zip(human_readable_date, average_stock_price))
-        return data
+        if json_data["status"] == "ERROR":
+            st.write("Too many request were created, maximum request is 5 per minute, try again a minute later")
+            raise RuntimeError
+        elif json_data["status"] == "NOT_AUTHORIZED":
+            st.write(
+                "Sorry, the range you have assigned contain too many steps! Please reduce the range of steps"
+                " by increasing the multiplier or decrease the date difference")
+            raise RuntimeError
+        else:
+            average_stock_price = [element["vw"] for element in json_data["results"]]
+            the_date_milliseconds = [element["t"] for element in json_data["results"]]
+            human_readable_date = [datetime.datetime.fromtimestamp(element / 1000).strftime('%Y-%m-%d %H:%M:%S') for
+                                   element
+                                   in the_date_milliseconds]
+            data = dict(zip(human_readable_date, average_stock_price))
+            return data
 
 
 st.header("Welcome to the Multi Select Chart Generator")
@@ -49,9 +57,12 @@ except KeyError:
     st.page_link("Mainpage.py", label="Home", icon="🏠")
     exit()
 except AssertionError:
-    st.error('The session item does not contain info, please go back to the main page to save your preference')
+    st.error('The session does not contain information, please go back to the main page to save your preference')
     st.page_link("Mainpage.py", label="Home", icon="🏠")
     exit()
+except Exception:
+    st.error("Unknown error have occurred please contact junwonjoon41@gmail.com, if the error persists")
+    raise RuntimeError
 else:
     st.subheader("Which company would you like to compare?")
     st.subheader("(choose up to 3)")
@@ -78,7 +89,10 @@ else:
             data_for_df = {f'Series{i}': [d[key] for key in keys] for i, d in enumerate(dfs)}
         except KeyError:
             st.write("Error in converting the data, please try again a minute later!")
-            exit()
+            raise RuntimeError
+        except Exception:
+            st.error("Unknown error have occurred please contact junwonjoon41@gmail.com, if the error persists")
+            raise RuntimeError
         else:
             df = pd.DataFrame(data_for_df, index=pd.to_datetime(keys))
             st.line_chart(df)
